0c28d038d4be035f0c9a0e8a14bf55b0825214a4
daiki.otsuka <tukadai1226@icloud.com>
2025-02-12 23:46:19 +0900

    計算式クイズアプリ作成

---
+++ MakeTheFormula/CalcViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CalcViewController.swift
+//  MakeTheFormula
+//
+//  Created by 大塚大樹 on 2025/02/12.
+//
+
+import UIKit
+
+class CalcViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var pulsButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var hangButton: UIButton!
+    @IBOutlet weak var devideButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ MakeTheFormula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeTheFormula
+//
+//  Created by 大塚大樹 on 2025/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
